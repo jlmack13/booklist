6d65c674f3b11826f67ca3efc0091960419acdd7
jlmack13 <jennimacklin@gmail.com>
2017-10-11 10:57:14 -0400

    added signup and logout routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,12 +21,32 @@ class UsersController < ApplicationController
 
   #logout
   get '/logout' do
-
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
   end
 
   #signup
   get '/signup' do
-    erb :'users/signup'
+    if !logged_in?
+      erb :'users/signup'
+    else
+      redirect to '/books'
+    end
+  end
+
+  post '/signup' do
+    if params[:name] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/books'
+    end
   end
 
 
