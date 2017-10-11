f9b4592a49306a5e7a87ab27c82d03e8345837aa
jlmack13 <jennimacklin@gmail.com>
2017-10-11 10:37:45 -0400

    added helper methods. built basic structure of users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,23 @@
 class UsersController < ApplicationController
-  
+
+  #login
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+
+  end
+
+  #logout
+  get '/logout' do
+
+  end
+
+  #signup
+  get '/signup' do
+    erb :'users/signup'
+  end
+
+
 end
