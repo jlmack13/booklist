c4f6412cde3a31fab87de02069fdc7d621d77253
jlmack13 <jennimacklin@gmail.com>
2017-10-12 11:14:46 -0400

    added basic styles to books view, added basic structure for books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,36 @@ class BooksController < ApplicationController
 
   #show user list of books
   get '/books' do
-    "Here are some books"
+    @user = current_user
+    erb :'books/index'
   end
+
+  #create new book
+  get '/books/new' do
+
+  end
+
+  post '/books' do
+
+  end
+
+  #show one book
+  get '/books/:id' do
+
+  end
+
+  #edit a book
+  get '/books/:id/edit' do
+
+  end
+
+  patch '/books/:id' do
+
+  end
+
+  #delete a book
+  get '/books/:id/delete' do
+    
+  end
+
 end
