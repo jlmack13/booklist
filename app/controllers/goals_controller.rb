a1aa0f481ccd8a3e2346b5d8194f39f3c25cb7fc
jlmack13 <jennimacklin@gmail.com>
2017-10-13 11:23:52 -0400

    adding get, and new controller actions to goals controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -3,17 +3,34 @@ class GoalsController < ApplicationController
   #show user list of goals
   #TO DO: Determine if I want the user to be able to see ALL of their old goals or just the current one?
   get '/goals' do
-    @user = current_user
-    erb :'goals/index'
+    if logged_in?
+      @user = current_user
+      @goals = @user.goals.all
+      erb :'goals/index'
+    else
+      redirect to '/'
+    end
   end
 
   #create new goal
   get '/goals/new' do
-
+    if logged_in?
+      erb :'goals/new'
+    else
+      redirect to '/'
+    end
   end
 
   post '/goals' do
-
+    if params[:target_number] == ""
+      redirect to '/goals/new'
+    else
+      previous_goal = Goal.find_by(active: true)
+      previous_goal.update(active: false)
+      @goal = current_user.goals.create(target_number: params[:target_number], active: true)
+      binding.pry
+      redirect to '/books'
+    end
   end
 
   #show one goal
