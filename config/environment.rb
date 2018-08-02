9c4010eaf5951872ce02ac10a2490c6359ace9b0
jlmack13 <jennimacklin@gmail.com>
2018-08-02 16:18:22 -0400

    attempting to deploy to heroku

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
 require 'bundler'
 Bundler.require
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/development.sqlite"
+  :adapter => "postgresql",
+  :database => "db/development"
 )
 
 require_all 'app'
